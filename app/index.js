edfdee9819f05ea32c621a7e4faddff2ae23c59f
rohin-arka <surya_siwakoti@outlook.com>
2017-05-19 14:53:56 +0545

    Create a class component App Import react, react-dom and index.css Create a class component  name App

---
+++ app/index.js
@@ -0,0 +1,11 @@
+// Importing react and react-dom modules
+import React from 'react';
+import ReactDOM from 'react-dom';
+// Importing index css inside app folder
+import './index.css';
+
+
+// class component App
+class App extends React.Component {
+
+}
