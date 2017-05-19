d9f15105f1bc8bff197a6681879751691161b0ea
rohin-arka <surya_siwakoti@outlook.com>
2017-05-19 15:57:08 +0545

    Tested by rendering hello world and finalize environment setup

--- app/index.js
+++ app/index.js
@@ -7,5 +7,9 @@ import './index.css';
 
 // class component App
 class App extends React.Component {
-
+ render(){
+   return <h1> hello world</h1>;
+ }
 }
+
+ReactDOM.render(<App />, document.getElementById('app'));
