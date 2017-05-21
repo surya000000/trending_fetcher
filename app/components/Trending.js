396f30cfbf9e3725ebde7453214f5b306d563fa3
rohin-arka <surya_siwakoti@outlook.com>
2017-05-21 18:59:35 +0545

    Created a trending component Render list of programming languages in li tag

--- app/components/Trending.js
+++ app/components/Trending.js
@@ -2,7 +2,29 @@
 import React from 'react';
 
 export default class Trending extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = { selected: 'All' };
+  }
+
+  updateState(language){
+    this.setState(function(){
+      return { selected: language, }
+    });
+  }
+
   render(){
-    return <div>Trending</div>
+    const languages = ['All', 'C', 'Ruby', 'Javascript', 'Python', 'Java', 'C++',
+                       'Swift']
+    return(
+      <ul className='programming-languages'>
+        <p>Selected Languages: {this.state.selected}</p>
+        {languages.map(function (language) {
+          return <li
+                  key={language}>
+                  {language}</li>
+        })}
+      </ul>
+    )
   }
 }
