37526d3e2817bcf7533ca964d97c2fded2bac61f
rohin-arka <surya_siwakoti@outlook.com>
2017-05-21 16:14:17 +0545

    Structure all components in components folder Shift app component to components folder with name App.js Export App class

---
+++ app/components/App.js
@@ -0,0 +1,9 @@
+// Importing react and react-dom modules
+import React from 'react';
+
+// class component App
+export default class App extends React.Component {
+ render(){
+   return <h1> hello world</h1>;
+ }
+}
