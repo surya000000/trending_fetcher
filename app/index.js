4e126a03fff04b0e6ce00365f2c7cd9c540da575
rohin-arka <surya_siwakoti@outlook.com>
2017-05-21 16:14:28 +0545

    Import app component in index.js

--- app/index.js
+++ app/index.js
@@ -1,15 +1,8 @@
 // Importing react and react-dom modules
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './components/App'
 // Importing index css inside app folder
 import './index.css';
-
-
-// class component App
-class App extends React.Component {
- render(){
-   return <h1> hello world</h1>;
- }
-}
 
 ReactDOM.render(<App />, document.getElementById('app'));
