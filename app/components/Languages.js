8e8888f0e56b413591edb2991c9856f29b3b3107
rohin-arka <surya_siwakoti@outlook.com>
2017-05-22 11:07:00 +0545

    Created Languages Component Created a stateless functional language component Language component return ui for list

---
+++ app/components/Languages.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Languages(props){
+  const programmingLanguages = ['All', 'C', 'Ruby', 'Javascript', 'Python', 'Java', 'C++',
+                     'Swift'];
+
+  return(<ul className='programming-languages'>
+         {programmingLanguages.map(function (language) {
+         return(<li key = {language}
+              onClick = {() => { props.updateLanguage(language) }}
+              className = { language == props.selected && 'active' }>
+              {language}
+         </li>)
+       })}
+     </ul>
+   )
+}
+export default Languages;
