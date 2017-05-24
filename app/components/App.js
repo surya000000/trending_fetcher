19a5b3ba1becd7be82485dd8fd8bbfcb45165c36
rohin-arka <surya_siwakoti@outlook.com>
2017-05-24 15:28:03 +0545

    Import React router modules

--- app/components/App.js
+++ app/components/App.js
@@ -1,13 +1,16 @@
 // Importing react and react-dom modules
 import React from 'react';
 import Trending from './Trending';
+var ReactRouter = require('react-router-dom');
+var Router = ReactRouter.BrowserRouter;
+var Route = ReactRouter.Route;
 // class component App
 export default class App extends React.Component {
  render(){
-   return(
-          <div className='container'>
-            <Trending />
-          </div>
-         )
+   return(<Router>
+            <div className='container'>
+              <Route path='/trending' component={Trending} />
+            </div>
+         </Router>)
  }
 }
