a1b96d40f342db515c341b2c9236222d381205aa
rohin-arka <surya_siwakoti@outlook.com>
2017-05-24 17:33:55 +0545

    Fix indendation

--- app/components/RepoList.js
+++ app/components/RepoList.js
@@ -2,27 +2,25 @@ import React from 'react';
 
 function RepoList(props){
   if(props.repos){
-    return (<ul className='repo-list'>
-                {props.repos.map(function(repo, index){
-                  return <div className='row'>
-                          <li key={repo.name} className='trending-repos'>
-                            <a href={repo.html_url} target='_blank'>
-                              <li key={index}> {"#"+(index+1)+" "+repo.name}</li>
-                              <li key={repo.owner.avatar_url}>
-                                <img src={repo.owner.avatar_url} className='repo-owner-img'/>
-                              </li>
-                              <li key={repo.stargazers_count} className='owner-details'>
-                                {"Star: "+repo.stargazers_count}
-                              </li>
-                              <li key={repo.owner.login}className='owner-details'>
-                                {"Github : " + repo.owner.login}
-                              </li>
-                            </a>
+    return(<ul className='repo-list'>
+            {props.repos.map(function(repo, index){
+             return(<div className='row'>
+                      <li key={repo.name} className='trending-repos'>
+                        <a href={repo.html_url} target='_blank'>
+                          <li key={index}> {"#"+(index+1)+" "+repo.name}</li>
+                          <li key={repo.owner.avatar_url}>
+                            <img src={repo.owner.avatar_url} className='repo-owner-img'/>
                           </li>
-                         </div>
-
-                })}
-              </ul>)
+                          <li key={repo.stargazers_count} className='owner-details'>
+                            {"Star: "+repo.stargazers_count}
+                          </li>
+                          <li key={repo.owner.login}className='owner-details'>
+                            {"Github : " + repo.owner.login}
+                          </li>
+                        </a>
+                      </li>
+                    </div>)})}
+          </ul>)
   }
   else{
     return <div className='loading'><h2 className='loading-text'>Loading.......</h2></div>
@@ -30,7 +28,3 @@ function RepoList(props){
 }
 
 export default RepoList;
-// {repo.owner.login}
-// {repo.name}
-// {repo.owner.login}
-// {repo.stargazers}
