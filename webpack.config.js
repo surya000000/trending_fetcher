9fc4faf15db290b3580b0a3fb521a10c490596b0
rohin-arka <surya_siwakoti@outlook.com>
2017-05-24 12:03:46 +0545

    Add configration for file loader for jpeg, gif and other image mime types

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,11 @@ module.exports = {
   module: {
     rules: [
       { test: /\.(js)$/, use: 'babel-loader' },
-      { test: /\.css$/, use: ['style-loader', 'css-loader'] }
+      { test: /\.css$/, use: ['style-loader', 'css-loader'] },
+      {
+       test: /\.(jpe?g|png|gif|svg)$/i,
+       use: "file-loader?name=/public/images/[name].[ext]"
+      }
     ]
   },
   plugins: [new HtmlWebPackPlugin({ template: 'app/index.html' })]
