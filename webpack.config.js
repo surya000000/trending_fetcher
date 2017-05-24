101c7eda4a8a1a043733bdf6db02412989e795ca
rohin-arka <surya_siwakoti@outlook.com>
2017-05-24 16:27:40 +0545

    Text messing up on long repo name fixed

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ var config = {
   output: {
     path: Path.resolve(__dirname, 'dist'),
     filename: 'application.js',
-    publicPath: '/public/'
+    publicPath: '/'
   },
   devServer: {
     historyApiFallback: true
